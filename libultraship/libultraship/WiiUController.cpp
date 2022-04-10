c5bf9cb10e36cfced0c739beef190bc3ac55e68f
Thomas Rohloff <v10lator@myway.de>
2022-04-10 11:59:36 +0200

    Support more controllers
    Signed-off-by: Thomas Rohloff <v10lator@myway.de>

--- libultraship/libultraship/WiiUController.cpp
+++ libultraship/libultraship/WiiUController.cpp
@@ -1,15 +1,19 @@
 #include "WiiUController.h"
 #include "GlobalCtx2.h"
 
+#include <padscore/kpad.h>
+#include <padscore/wpad.h>
 #include <vpad/input.h>
 
 namespace Ship {
 	WiiUController::WiiUController(int32_t dwControllerNumber) : Controller(dwControllerNumber) {
+		KPADInit();
+		WPADEnableURCC(true);
 		LoadBinding();
 	}
 
 	WiiUController::~WiiUController() {
-		
+		KPADShutdown();
 	}
 
 	void WiiUController::ReadFromSource() {
@@ -57,6 +61,116 @@ namespace Ship {
 			wStickY = status.leftStick.y * 84;
 
 			// TODO gyro
+		}
+
+		KPADStatus kStatus;
+		int32_t kProbe;
+		WPADExtensionType kType;
+
+		for (int i = 0; i < 4; i++) {
+			kProbe = WPADProbe((WPADChan) i, &kType);
+
+			if (kProbe)
+				continue;
+
+			KPADRead((WPADChan) i, &kStatus, 1);
+
+			switch (kType) {
+				case WPAD_EXT_PRO_CONTROLLER:
+					if (kStatus.pro.hold & WPAD_PRO_BUTTON_A)
+						dwPressedButtons |= BTN_A;
+					if (kStatus.pro.hold & WPAD_PRO_BUTTON_B)
+						dwPressedButtons |= BTN_B;
+					if (kStatus.pro.hold & WPAD_PRO_TRIGGER_ZR)
+						dwPressedButtons |= BTN_Z;
+					if (kStatus.pro.hold & WPAD_PRO_TRIGGER_R)
+						dwPressedButtons |= BTN_R;
+					if (kStatus.pro.hold & WPAD_PRO_TRIGGER_L)
+						dwPressedButtons |= BTN_L;
+					if (kStatus.pro.hold & WPAD_PRO_BUTTON_PLUS)
+						dwPressedButtons |= BTN_START;
+					if (kStatus.pro.hold & WPAD_PRO_BUTTON_UP)
+						dwPressedButtons |= BTN_DUP;
+					if (kStatus.pro.hold & WPAD_PRO_BUTTON_DOWN)
+						dwPressedButtons |= BTN_DDOWN;
+					if (kStatus.pro.hold & WPAD_PRO_BUTTON_RIGHT)
+						dwPressedButtons |= BTN_DRIGHT;
+					if (kStatus.pro.hold & WPAD_PRO_BUTTON_LEFT)
+						dwPressedButtons |= BTN_DLEFT;
+
+					if (kStatus.pro.rightStick.x > 0.2f)
+						dwPressedButtons |= BTN_CRIGHT;
+					if (kStatus.pro.rightStick.x < -0.2f)
+						dwPressedButtons |= BTN_CLEFT;
+					if (kStatus.pro.rightStick.y > 0.2f)
+						dwPressedButtons |= BTN_CUP;
+					if (kStatus.pro.rightStick.y < -0.2f)
+						dwPressedButtons |= BTN_CDOWN;
+
+					wStickX += kStatus.pro.leftStick.x * 84;
+					wStickY += kStatus.pro.leftStick.y * 84;
+					break;
+				case WPAD_EXT_CLASSIC:
+				case WPAD_EXT_MPLUS_CLASSIC:
+					if (kStatus.classic.hold & WPAD_CLASSIC_BUTTON_A)
+						dwPressedButtons |= BTN_A;
+					if (kStatus.classic.hold & WPAD_CLASSIC_BUTTON_B)
+						dwPressedButtons |= BTN_B;
+					if (kStatus.classic.hold & WPAD_CLASSIC_BUTTON_ZR)
+						dwPressedButtons |= BTN_Z;
+					if (kStatus.classic.hold & WPAD_CLASSIC_BUTTON_R)
+						dwPressedButtons |= BTN_R;
+					if (kStatus.classic.hold & WPAD_CLASSIC_BUTTON_L)
+						dwPressedButtons |= BTN_L;
+					if (kStatus.classic.hold & WPAD_CLASSIC_BUTTON_PLUS)
+						dwPressedButtons |= BTN_START;
+					if (kStatus.classic.hold & WPAD_CLASSIC_BUTTON_UP)
+						dwPressedButtons |= BTN_DUP;
+					if (kStatus.classic.hold & WPAD_CLASSIC_BUTTON_DOWN)
+						dwPressedButtons |= BTN_DDOWN;
+					if (kStatus.classic.hold & WPAD_CLASSIC_BUTTON_RIGHT)
+						dwPressedButtons |= BTN_DRIGHT;
+					if (kStatus.classic.hold & WPAD_CLASSIC_BUTTON_LEFT)
+						dwPressedButtons |= BTN_DLEFT;
+
+					if (kStatus.classic.hold & WPAD_CLASSIC_STICK_R_EMULATION_RIGHT)
+						dwPressedButtons |= BTN_CRIGHT;
+					else if (kStatus.classic.hold & WPAD_CLASSIC_STICK_R_EMULATION_LEFT)
+						dwPressedButtons |= BTN_CLEFT;
+					if (kStatus.classic.hold & WPAD_CLASSIC_STICK_R_EMULATION_UP)
+						dwPressedButtons |= BTN_CUP;
+					else if (kStatus.classic.hold & WPAD_CLASSIC_STICK_R_EMULATION_DOWN)
+						dwPressedButtons |= BTN_CDOWN;
+
+					wStickX += kStatus.classic.leftStick.x * 84;
+					wStickY += kStatus.classic.leftStick.y * 84;
+					break;
+				case WPAD_EXT_NUNCHUK:
+				case WPAD_EXT_MPLUS_NUNCHUK:
+					wStickX += kStatus.nunchuck.stick.x * 84;
+					wStickY += kStatus.nunchuck.stick.y * 84;
+			}
+
+			if (kStatus.hold & WPAD_BUTTON_A)
+				dwPressedButtons |= BTN_A;
+			if (kStatus.hold & WPAD_BUTTON_B)
+				dwPressedButtons |= BTN_B;
+			if (kStatus.hold & WPAD_BUTTON_MINUS)
+				dwPressedButtons |= BTN_Z;
+			if (kStatus.hold & WPAD_BUTTON_1)
+				dwPressedButtons |= BTN_R;
+			if (kStatus.hold & WPAD_BUTTON_2)
+				dwPressedButtons |= BTN_L;
+			if (kStatus.hold & WPAD_BUTTON_PLUS)
+				dwPressedButtons |= BTN_START;
+			if (kStatus.hold & WPAD_BUTTON_UP)
+				dwPressedButtons |= BTN_DUP;
+			if (kStatus.hold & WPAD_BUTTON_DOWN)
+				dwPressedButtons |= BTN_DDOWN;
+			if (kStatus.hold & WPAD_BUTTON_RIGHT)
+				dwPressedButtons |= BTN_DRIGHT;
+			if (kStatus.hold & WPAD_BUTTON_LEFT)
+				dwPressedButtons |= BTN_DLEFT;
 		}
 	}
 
