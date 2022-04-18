c6963b671e04d428fad73b82a3e9a0edd6c7e058
Thomas Rohloff <v10lator@myway.de>
2022-04-18 13:03:19 +0200

    Register new CVars
    Signed-off-by: Thomas Rohloff <v10lator@myway.de>

--- soh/soh/Enhancements/bootcommands.c
+++ soh/soh/Enhancements/bootcommands.c
@@ -26,6 +26,9 @@ void BootCommands_Init()
     CVar_RegisterS32("gPauseLiveLink", 0);
     CVar_RegisterS32("gMinimalUI", 0);
     CVar_RegisterS32("gNewDrops", 1);
+    CVar_RegisterS32("gRumbleEnabled", 1);
+    CVar_RegisterS32("gRumbleEnabled", 1);
+    CVar_RegisterS32("gTextSpeed", 3);
 }
 
 //void BootCommands_ParseBootArgs(char* str)
