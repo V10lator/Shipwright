f3f2c6f88748fc5b575df34a7a7e0c3f66fbff31
Thomas Rohloff <v10lator@myway.de>
2022-04-18 13:03:19 +0200

    Register new CVars
    Signed-off-by: Thomas Rohloff <v10lator@myway.de>

--- soh/soh/Enhancements/bootcommands.c
+++ soh/soh/Enhancements/bootcommands.c
@@ -30,6 +30,7 @@ void BootCommands_Init()
     CVar_RegisterS32("gNewDrops", 1);
     CVar_RegisterS32("gVisualAgony", 0);
     CVar_RegisterS32("gLanguages", 0); //0 = English / 1 = German / 2 = French
+    CVar_RegisterS32("gTextSpeed", 3);
 }
 
 //void BootCommands_ParseBootArgs(char* str)
